fa6f4e5a47381809a316efc4c47f1a6cfc2409f4
SandeepMS07 <sandeepms746@gmail.com>
2022-04-16 16:08:59 +0530

    src code

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  mode: "jit",
+  content: [
+    "./pages/**/*.{html,js,ts,jsx,tsx}",
+    "./components/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
