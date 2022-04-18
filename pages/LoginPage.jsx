d55d6ce91bf68195ea0dc21a3b8aed1aa059de5b
SandeepMS07 <sandeepms746@gmail.com>
2022-04-18 18:37:32 +0530

    adding dashboard page and login page

---
+++ pages/LoginPage.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import Link from "next/link";
+
+import { FaRegEnvelope } from "react-icons/fa";
+import { MdLockOutline } from "react-icons/md";
+import { useState } from "react";
+import { Router } from "next/router";
+
+const LoginPage = () => {
+  const [formvalues, setFormvalues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, seterror] = useState({});
+  const [issubmit, setIssubmit] = useState(false);
+
+  const handleClick = (e) => {
+    // e.preventDefault();
+    seterror(valid(formvalues));
+
+    setIssubmit(true);
+  };
+  const updateData = (e) => {
+    let { name, value } = e.target;
+    setFormvalues({ ...formvalues, [name]: value });
+    seterror(valid(formvalues));
+  };
+
+  let valid = (values) => {
+    let errors = {};
+
+    if (!values.email) {
+      errors.email = "*Email required";
+    }
+    if (!values.password) {
+      errors.password = "*password Required";
+    }
+    return errors;
+  };
+  return (
+    <div>
+      <div className="flex items-center justify-center w-full  text-center">
+        <div className="bg-white rounded-2xl shadow-2xl flex items-center justify-center w-65 md:w-96 max-w-4xl">
+          {/*  */}
+          <div className="p-4 md:p-5">
+            <div className="p-6 md:p-10">
+              <h2 className="text-2xl md:text-3xl font-bold text-green-600 mb-1 md:mb-2">
+                Sign in to Account
+              </h2>
+
+              <div className=" border-2 w-8 md:w-12 bg-green-600 border-green-600 inline-block mb-2"></div>
+
+              <form action="" className="flex flex-col items-center mb-2">
+                <div className="bg-gray-100 md:w-64 p-1 md:p-2 flex items-center mb-3">
+                  <FaRegEnvelope className="text-gray-400 m-2" />
+                  <input
+                    type="email"
+                    name="email"
+                    placeholder="Email"
+                    className="bg-gray-100 outline-none text-sm flex-1"
+                    onChange={updateData}
+                  />
+                </div>
+                <p className="text-xs mb-2 text-red-600 ">{error.email}</p>
+
+                <div className="bg-gray-100  md:w-64 p-1 md:p-2 flex items-center mb-3">
+                  <MdLockOutline className="text-gray-400 m-2" />
+                  <input
+                    type="password"
+                    name="password"
+                    placeholder="Password"
+                    className="bg-gray-100 outline-none text-sm flex-1"
+                    onChange={updateData}
+                  />
+                </div>
+                <p className="text-xs mb-2 text-red-600 ">{error.password}</p>
+                <Link href="/Dashboard/" passHref>
+                  <button
+                    type="submit"
+                    className="border-2 border-green-500 text-green-500 rounded-full px-8 md:px-12 py-1 md:py-2 inline-block font-semibold hover:bg-green-500 hover:text-white"
+                    onClick={handleClick}
+                  >
+                    Sign in
+                  </button>
+                </Link>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
